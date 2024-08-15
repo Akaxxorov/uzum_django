a857fce2b2563c7382e228de121fafc3e0bd981f
abduqahhorov Aziz <azizabduqahhorov07@gmail.com>
2024-08-15 16:40:58 +0000

    yangliklar

--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+from django.db.models import Model , CharField ,ImageField , PositiveIntegerField ,ForeignKey, FloatField, TextField, IntegerField,CASCADE
 
-# Create your models here.
+
+class Category(Model):
+    name = CharField(max_length=5000)
+    # parent = 
+
+
+    def __str__(self):
+        return self.name
+
+
+class Product(Model):
+    name = CharField(max_length=5000)
+    narxi = FloatField()
+    discount = PositiveIntegerField()
+    rang = CharField(max_length=200)
+    short_description = TextField()
+    description = TextField()
+    quantity = IntegerField()
+    category = ForeignKey(Category, on_delete=CASCADE)
+
+
+class ProductImage(Model):
+    image = ImageField()
+    Product = ForeignKey(Product, on_delete=CASCADE)
