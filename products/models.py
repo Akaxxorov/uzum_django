4299bc5adb40517633030cf580986fb0b7b06da7
abduqahhorov Aziz <azizabduqahhorov07@gmail.com>
2024-08-15 17:57:20 +0000

    model fayli qo'shildi

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db.models import Model , CharField ,ImageField , PositiveIntegerFiel
 
 class Category(Model):
     name = CharField(max_length=5000)
-    # parent = 
+    parent = ForeignKey('self',CASCADE, null=True, blank=True)
 
 
     def __str__(self):
@@ -14,13 +14,13 @@ class Product(Model):
     name = CharField(max_length=5000)
     narxi = FloatField()
     discount = PositiveIntegerField()
-    rang = CharField(max_length=200)
+    rang = CharField(max_length=200, null=True,blank=True)
     short_description = TextField()
     description = TextField()
-    quantity = IntegerField()
-    category = ForeignKey(Category, on_delete=CASCADE)
+    quantity = PositiveIntegerField(default=1)
+    category = ForeignKey(Category, on_delete=CASCADE,related_name='products')
 
 
 class ProductImage(Model):
-    image = ImageField()
-    Product = ForeignKey(Product, on_delete=CASCADE)
+    image = ImageField(upload_to='products')
+    Product = ForeignKey(Product, on_delete=CASCADE, related_name='images')
